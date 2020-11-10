da7916b8cb3e4e129bf5cb843562dda617b02d48
Polson <polson.k@huawei.com>
2020-11-10 17:28:15 +0700

    implement FdlResponse

--- firebase-dynamic-links-rest/src/main/java/com/anotherdev/firebase/dynamic/links/FdlResponse.java
+++ firebase-dynamic-links-rest/src/main/java/com/anotherdev/firebase/dynamic/links/FdlResponse.java
@@ -1,4 +1,36 @@
 package com.anotherdev.firebase.dynamic.links;
 
+import com.google.gson.annotations.SerializedName;
+
+import org.immutables.gson.Gson;
+import org.immutables.value.Value;
+
+import java.util.List;
+
+@Value.Immutable
+@Value.Style(strictBuilder = true)
+@Gson.TypeAdapters
 interface FdlResponse {
+
+    @SerializedName("shortLink")
+    String getShortLink();
+
+    @SerializedName("previewLink")
+    String getPreviewLink();
+
+    @SerializedName("warning")
+    List<Warning> getWarnings();
+
+
+    @Value.Immutable
+    @Value.Style(strictBuilder = true)
+    @Gson.TypeAdapters
+    interface Warning {
+
+        @SerializedName("warningCode")
+        String getWarningCode();
+
+        @SerializedName("warningMessage")
+        String getWarningMessage();
+    }
 }
