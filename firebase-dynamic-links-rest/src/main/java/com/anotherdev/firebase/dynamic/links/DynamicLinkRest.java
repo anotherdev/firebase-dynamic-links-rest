ee7d56ac0ec306f7a7db3d2b1295297cac5be791
Polson <polson.k@huawei.com>
2020-11-10 14:23:47 +0700

    add DynamicLinkRest builder

---
+++ firebase-dynamic-links-rest/src/main/java/com/anotherdev/firebase/dynamic/links/DynamicLinkRest.java
@@ -0,0 +1,40 @@
+package com.anotherdev.firebase.dynamic.links;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.dynamiclinks.DynamicLink;
+import com.google.firebase.dynamiclinks.ShortDynamicLink;
+
+import org.immutables.value.Value;
+
+@Value.Immutable
+@Value.Style(
+        strictBuilder = true
+)
+public interface DynamicLinkRest {
+
+    DynamicLink.AndroidParameters.Builder getAndroidParametersBuilder();
+    DynamicLink.SocialMetaTagParameters.Builder getSocialMetaTagParametersBuilder();
+    DynamicLink.Builder getDynamicLinkBuilder();
+
+
+    class Builder extends ImmutableDynamicLinkRest.Builder {
+
+        @NonNull
+        private final FirebaseDynamicLinksRest firebase;
+
+        Builder() {
+            firebase = FirebaseDynamicLinksRest.getInstance();
+        }
+
+        Builder(@NonNull FirebaseDynamicLinksRest firebase) {
+            this.firebase = firebase;
+        }
+
+        @NonNull
+        public Task<ShortDynamicLink> buildShortDynamicLink(int suffix) {
+            return firebase.buildShortDynamicLink(suffix);
+        }
+    }
+}
