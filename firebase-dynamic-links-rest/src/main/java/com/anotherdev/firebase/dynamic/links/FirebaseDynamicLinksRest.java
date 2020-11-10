5c9c15cae52892cde55b6a0d08e4702908918a33
Polson <polson.k@huawei.com>
2020-11-10 16:14:14 +0700

    add OkHttp logging interceptor

--- firebase-dynamic-links-rest/src/main/java/com/anotherdev/firebase/dynamic/links/FirebaseDynamicLinksRest.java
+++ firebase-dynamic-links-rest/src/main/java/com/anotherdev/firebase/dynamic/links/FirebaseDynamicLinksRest.java
@@ -1,6 +1,8 @@
 package com.anotherdev.firebase.dynamic.links;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
@@ -8,10 +10,19 @@ import com.chuckerteam.chucker.api.ChuckerInterceptor;
 import com.google.firebase.FirebaseApp;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static okhttp3.logging.HttpLoggingInterceptor.Level.BASIC;
+import static okhttp3.logging.HttpLoggingInterceptor.Level.BODY;
+
 public class FirebaseDynamicLinksRest {
+
+    private static final String TAG = FirebaseDynamicLinksRest.class.getName();
 
     private static FdlApi api;
 
@@ -34,8 +45,10 @@ public class FirebaseDynamicLinksRest {
     @NonNull
     private FdlApi getApi(@NonNull Context context) {
         if (api == null) {
+            final boolean isDebugBuild = (context.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
             OkHttpClient client = new OkHttpClient.Builder()
                     .addInterceptor(new ChuckerInterceptor.Builder(context).build())
+                    .addInterceptor(new HttpLoggingInterceptor().setLevel(isDebugBuild ? BODY : BASIC))
                     .addInterceptor(new FdlAuthInterceptor(context))
                     .build();
 
