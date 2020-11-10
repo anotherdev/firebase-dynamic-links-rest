c2c97869f98e65d2d4fec420388b03b18d0c2a4b
Polson <polson.k@huawei.com>
2020-11-10 10:46:36 +0700

    add suffix

--- firebase-dynamic-links-rest/src/main/java/com/anotherdev/firebase/dynamic/links/FdlRequest.java
+++ firebase-dynamic-links-rest/src/main/java/com/anotherdev/firebase/dynamic/links/FdlRequest.java
@@ -1,5 +1,7 @@
 package com.anotherdev.firebase.dynamic.links;
 
+import androidx.annotation.Nullable;
+
 import com.google.gson.annotations.SerializedName;
 
 import org.immutables.gson.Gson;
@@ -10,8 +12,13 @@ import org.immutables.value.Value;
 @Gson.TypeAdapters
 interface FdlRequest {
 
+    @Nullable
     @SerializedName("dynamicLinkInfo")
     DynamicLinkInfo getDynamicLinkInfo();
+
+    @Nullable
+    @SerializedName("suffix")
+    Suffix getSuffix();
 
     static ImmutableFdlRequest.Builder builder() {
         return ImmutableFdlRequest.builder();
@@ -28,6 +35,29 @@ interface FdlRequest {
 
         static ImmutableDynamicLinkInfo.Builder builder() {
             return ImmutableDynamicLinkInfo.builder();
+        }
+    }
+
+
+    @Value.Immutable
+    @Value.Style(strictBuilder = true)
+    @Gson.TypeAdapters
+    interface Suffix {
+
+        @Value.Default
+        @SerializedName("option")
+        default Option getOption() {
+            return Option.UNGUESSABLE;
+        }
+
+        static ImmutableSuffix.Builder builder() {
+            return ImmutableSuffix.builder();
+        }
+
+
+        enum Option {
+            UNGUESSABLE,
+            SHORT
         }
     }
 }
